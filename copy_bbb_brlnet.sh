3938857ef348e0b545963ea078208169364dfcd5
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-26 21:16:48 +0000

    updated the copy files

--- copy_bbb_brlnet.sh
+++ copy_bbb_brlnet.sh
@@ -5,7 +5,7 @@ rsync -avzh ./src/*.cpp 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/sr
 rsync -avzh ./include/thruster_ident_driver/*.hpp 	BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/include/thruster_ident_driver/
 rsync -avzh CMakeLists.txt 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 rsync -avzh *.xml 								BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
-rsync -avzh *.launch	 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
+rsync -avzh ./launch/*.launch	 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/launch
 rsync -avzh *.yaml		 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 rsync -avzh *.md								BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 
--- copy_bbb_brlwifi.sh
+++ copy_bbb_brlwifi.sh
@@ -5,7 +5,7 @@ rsync -avzh ./src/*.cpp 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/s
 rsync -avzh ./include/thruster_ident_driver/*.hpp 	BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/include/thruster_ident_driver/
 rsync -avzh CMakeLists.txt 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 rsync -avzh *.xml 								BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
-rsync -avzh *.launch	 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
+rsync -avzh ./lauch/*.launch	 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/launch/
 rsync -avzh *.yaml		 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 rsync -avzh *.md								BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/thruster_ident_driver/
 
